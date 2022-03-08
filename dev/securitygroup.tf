b8f6394282635426ff76849a8259a5c4b9628705
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2022-03-07 18:45:24 -0800

    Create securitygroup.tf

---
+++ dev/securitygroup.tf
@@ -0,0 +1,73 @@
+#ALB security group 
+resource "aws_security_group" "cw_alb" {
+  name   = "${var.name}-alb"
+  vpc_id = aws_vpc.app_vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "cw_launch_config" {
+  name   = "${var.name}-service"
+  vpc_id = aws_vpc.app_vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 51678
+    to_port     = 51678
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
